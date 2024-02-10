e85c7d9f0fbbc1908ccdbbf3da2d275d71ac5ba9
Bogdan Vlad <bogdan@genez.io>
2024-02-10 18:14:52 +0200

    Initial commit

---
+++ server/backend.ts
@@ -0,0 +1,12 @@
+import { GenezioDeploy, GenezioMethod } from "@genezio/types";
+
+
+@GenezioDeploy()
+export class BackendService {
+  readonly secret = "Capybaras are AWESOME! Shhh... don't tell the cats!";
+
+  @GenezioMethod()
+  async getSecret() {
+    return this.secret;
+  }
+}
