7ee9140b914f21e73bb0295679df66c82b9bae81
Bogdan Vlad <bogdan@genez.io>
2024-02-19 18:03:26 +0200

    Minor fixes

--- server/backend.ts
+++ server/backend.ts
@@ -1,4 +1,4 @@
-import { GenezioDeploy, GenezioMethod, GnzAuth, GnzContext } from "@genezio/types";
+import { GenezioDeploy, GenezioMethod, GenezioAuth, GnzContext } from "@genezio/types";
 
 
 @GenezioDeploy()
@@ -6,7 +6,7 @@ export class BackendService {
   readonly secret = "Capybaras are AWESOME! Shhh... don't tell the cats!";
 
   @GenezioMethod()
-  @GnzAuth()
+  @GenezioAuth()
   async getSecret(context: GnzContext) {
       console.log(context.user);
       return this.secret;
